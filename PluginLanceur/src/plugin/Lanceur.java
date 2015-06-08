bcee85262e75800054166949ec13b158561617f4
SebDuboisMIAGE <dubois.sebastien38@gmail.com>
2015-06-08 23:22:10 +0200

    Plugin Principal fonctionnel !!

--- PluginLanceur/src/plugin/Lanceur.java
+++ PluginLanceur/src/plugin/Lanceur.java
@@ -1,5 +1,21 @@
 package plugin;
 
-public class Lanceur {
+import core.Platform;
+import interfaces.IPlugin;
 
+public class Lanceur implements IPlugin{
+	
+	public void chargerPlugin()
+	{
+		System.out.println("Vous êtes dans le plugin Principal");
+		Platform plateforme = Platform.getInstance();
+		/*
+		 * Appel de la création de la map
+		 * Récupération des Producer 
+		 */
+		
+		/*
+		 * Appel de la plateforme et affichage des plugin disponibles
+		 */
+	}
 }
